6c3752d7210de67a9d12b052f17aa7b6547755bc
WhosJohnGault <Bravesboy1116@gmail.com>
2017-03-02 11:35:32 -0800

    Add files via upload

---
+++ CPPTemplate/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: James Jordan
+ * Created on MArch 3, 2017, 11:25 AM
+ * Purpose: To make my life somewhat easier
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    
+
+    //Exit
+    return 0;
+}
+
